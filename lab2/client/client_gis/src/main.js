6182e61e57e7d4db3aa43aaea9132f64e5c5f9f7
Colnuwko <Vladislav290403@gmail.com>
2026-05-20 19:41:05 +0400

    Еще файлы 3 лабы

--- lab2/client/client_gis/src/main.js
+++ lab2/client/client_gis/src/main.js
@@ -1,22 +1,31 @@
 import 'ol/ol.css';
+
 import Map from 'ol/Map';
 import View from 'ol/View';
+
 import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
 import ImageLayer from 'ol/layer/Image';
+import VectorLayer from 'ol/layer/Vector';
+
+import OSM from 'ol/source/OSM';
 import ImageWMS from 'ol/source/ImageWMS';
+import VectorSource from 'ol/source/Vector';
+
+import GeoJSON from 'ol/format/GeoJSON';
 import {fromLonLat} from "ol/proj.js";
+import { applyStyle } from 'ol-mapbox-style';
 
 const wmsUrl = 'http://localhost:8080/geoserver/gis/wms';
 
-// Слой "Здания"
+
+
 const buildingsLayer = new ImageLayer({
   source: new ImageWMS({
     url: wmsUrl,
     params: {
-      'LAYERS': 'gis:buildings',
-      'TILED': true,
-      'FORMAT': 'image/png'
+      LAYERS: 'gis:buildings',
+      TILED: true,
+      FORMAT: 'image/png'
     },
     ratio: 1,
     serverType: 'geoserver'
@@ -24,14 +33,13 @@ const buildingsLayer = new ImageLayer({
   visible: true
 });
 
-// Слой "Дороги"
 const roadsLayer = new ImageLayer({
   source: new ImageWMS({
     url: wmsUrl,
     params: {
-      'LAYERS': 'gis:roads',
-      'TILED': true,
-      'FORMAT': 'image/png'
+      LAYERS: 'gis:roads',
+      TILED: true,
+      FORMAT: 'image/png'
     },
     ratio: 1,
     serverType: 'geoserver'
@@ -39,14 +47,13 @@ const roadsLayer = new ImageLayer({
   visible: true
 });
 
-// Слой "POI" (точки интереса)
 const poiLayer = new ImageLayer({
   source: new ImageWMS({
     url: wmsUrl,
     params: {
-      'LAYERS': 'gis:poi',
-      'TILED': true,
-      'FORMAT': 'image/png'
+      LAYERS: 'gis:poi',
+      TILED: true,
+      FORMAT: 'image/png'
     },
     ratio: 1,
     serverType: 'geoserver'
@@ -54,32 +61,72 @@ const poiLayer = new ImageLayer({
   visible: true
 });
 
+
+
+const overtureLayer = new VectorLayer({
+  source: new VectorSource({
+    url: './overture.geojson',
+    format: new GeoJSON()
+  }),
+  visible: true
+});
+
+
+
+applyStyle(overtureLayer, './style.json');
+//
+// Карта
+//
+
 const map = new Map({
   target: 'map',
+
   layers: [
     new TileLayer({
       source: new OSM()
     }),
+
     buildingsLayer,
     roadsLayer,
-    poiLayer
+    poiLayer,
+    overtureLayer
   ],
+
   view: new View({
-    center: fromLonLat([50.482017517089844, 54.29159164428711]),
+    center: [50.4820175, 54.2915916],
     zoom: 15,
-    projection: 'EPSG:3857'
+    projection: 'EPSG:4326'
   })
 });
 
-// Управление видимостью через кнопки
-document.getElementById('toggleBuildings').addEventListener('click', () => {
-  buildingsLayer.setVisible(!buildingsLayer.getVisible());
-});
+//
+// Кнопки управления слоями
+//
 
-document.getElementById('toggleRoads').addEventListener('click', () => {
-  roadsLayer.setVisible(!roadsLayer.getVisible());
-});
+document.getElementById('toggleBuildings')
+  .addEventListener('click', () => {
+    buildingsLayer.setVisible(
+      !buildingsLayer.getVisible()
+    );
+  });
 
-document.getElementById('togglePoi').addEventListener('click', () => {
-  poiLayer.setVisible(!poiLayer.getVisible());
-});
+document.getElementById('toggleRoads')
+  .addEventListener('click', () => {
+    roadsLayer.setVisible(
+      !roadsLayer.getVisible()
+    );
+  });
+
+document.getElementById('togglePoi')
+  .addEventListener('click', () => {
+    poiLayer.setVisible(
+      !poiLayer.getVisible()
+    );
+  });
+
+document.getElementById('toggleOverture')
+  .addEventListener('click', () => {
+    overtureLayer.setVisible(
+      !overtureLayer.getVisible()
+    );
+  });
---
+++ lab3/scripts.sql
@@ -0,0 +1,85 @@
+INSTALL spatial;
+LOAD spatial;
+
+INSTALL httpfs;
+LOAD httpfs;
+
+CREATE OR REPLACE TABLE user_buildings AS
+SELECT
+    geom::GEOMETRY AS geom,
+    id,
+    building
+FROM ST_Read('C:\Users\Colnuwko\source\repos\Colnuwko\gis-2026\lab3\GeoJson.geojson');
+
+
+SELECT COUNT(*) FROM user_buildings;
+
+SELECT
+    ST_XMin(ST_Extent_Agg(geom)) AS xmin,
+    ST_YMin(ST_Extent_Agg(geom)) AS ymin,
+    ST_XMax(ST_Extent_Agg(geom)) AS xmax,
+    ST_YMax(ST_Extent_Agg(geom)) AS ymax
+FROM user_buildings;
+
+
+SET s3_region='us-west-2';
+SET http_timeout = 300000;
+SET http_retries = 5;
+SET threads = 4;
+
+CREATE OR REPLACE TABLE overture_raw AS
+SELECT
+    id,
+    names,
+    sources,
+    subtype,
+    class,
+    geometry
+FROM read_parquet(
+    's3://overturemaps-us-west-2/release/2026-04-15.0/theme=buildings/type=building/*.zstd.parquet'
+)
+WHERE
+    bbox.xmin <= 50.5192718
+    AND bbox.xmax >= 50.4576249
+    AND bbox.ymin <= 54.3473813
+    AND bbox.ymax >= 54.2641987;
+
+SELECT COUNT(*) FROM overture_raw;
+
+CREATE OR REPLACE TABLE overture_buildings AS
+SELECT
+    o.id,
+    o.geometry AS geom,
+    o.subtype,
+    o.class,
+
+    CASE
+        WHEN EXISTS (
+            SELECT 1
+            FROM user_buildings u
+            WHERE ST_Intersects(o.geometry, u.geom)
+        )
+        THEN 'my'
+
+        WHEN LOWER(CAST(o.sources AS VARCHAR)) LIKE '%osm%'
+        THEN 'osm'
+
+        ELSE 'ml'
+    END AS source_type
+
+FROM overture_raw o;
+
+COPY (
+    SELECT
+        id,
+        source_type,
+        subtype,
+        class,
+        geom
+    FROM overture_buildings
+)
+TO 'C:\Users\Colnuwko\source\repos\Colnuwko\gis-2026\lab2\client\client_gis\public\overture.geojson'
+WITH (
+    FORMAT GDAL,
+    DRIVER 'GeoJSON'
+);
